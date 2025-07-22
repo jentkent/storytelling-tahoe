e13bf30cae9d38bd3e1d97aa02a0c8488f8b2c4f
jentkent <jenniferkent@unr.edu>
2025-07-22 11:36:20 -0700

    Major updates
    Round 1

--- config.js
+++ config.js
@@ -22,11 +22,11 @@ var config = {
     chapters: [
         {
             id: 'chapter-1-tahoe',
-            alignment: 'left',
+            alignment: 'center',
             hidden: false,
             title: 'What is the Tahoe Environmental Observatory Network?',
-            image: './assets/san-fran.jpeg',
-            description: 'Lake Tahoe is known for its crystal clear waters and stunning vistas. For decades, conservation efforts have focused on the lake itself, monitoring and protecting its fragile clarity.',
+            image: './assets/tahoe-scenic.jpeg',
+            description: 'Lake Tahoe is known for its crystal clear waters and stunning vistas. For decades, conservation efforts have focused on the lake itself, <span style="color: #dcc581;"><strong>monitoring and protecting its fragile clarity.</strong></span>',
             location: {
                 center: [-120.0324, 39.0968],
                 zoom: 10.5,
@@ -75,8 +75,8 @@ var config = {
             id: 'enter-tahoe-basin',
             alignment: 'right',
             hidden: false,
-            image: './assets/washington-dc.jpg',
-            description: 'But a group of scientists is now looking beyond the lake to protect it. Growing threats like climate change, invasive species, wildfires, forest shifts, and altered stream flows are placing new pressures on the Tahoe Basin. <br>  <br> Preserving this fragile ecosystem requires urgent, science-driven action.',
+            image: './assets/research-sudeep.jpg',
+            description: 'But a group of scientists is now looking beyond the lake to protect it. Growing threats like climate change, invasive species, wildfires, forest shifts, and altered stream flows are placing new pressures on the Tahoe Basin. <br>  <br> Preserving this fragile ecosystem requires <span style="color: #dcc581;"><strong> urgent, science-driven action. </strong></span>',
             location: {
                center: [-119.967296, 39.035172],
                 zoom: 9.5,
@@ -106,7 +106,7 @@ var config = {
             id: 'enter-water-monitoring',
             alignment: 'right',
             hidden: false,
-            image: './assets/geneva.jpg',
+            image: './assets/research-nearshore.jpg',
             description: 'The Tahoe Environmental Observatory Network is monitoring water quality in the nearshore,',
             location: {
                 center: [-119.940589, 39.080105],
@@ -134,7 +134,7 @@ var config = {
             id: 'enter-terrestrial-monitoring',
             alignment: 'right',
             hidden: false,
-            image: './assets/buenos-aires.jpg',
+            image: './assets/research-land.jpg',
             description: 'while also tracking changes across the Tahoe Basin, like forest and soil health, changes in climate, and wildlife activity,',
             location: {
                 center: [-119.940589, 39.080105],
@@ -162,14 +162,13 @@ var config = {
         },
         {
             id: 'enter-stream-monitoring',
-            alignment: 'left',
+            alignment: 'right',
             hidden: false,
-            title: 'Stream Monitoring',
-            image: './assets/buenos-aires.jpg',
-            description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
+            image: './assets/research-streams.jpg',
+            description: 'as well as changes in the streams that feed the lake.',
             location: {
-                center: [-120.154247, 39.077802],
-                zoom: 11,
+                center: [-119.940589, 39.080105],
+                zoom: 10,
                 pitch: 0,
                 bearing: 0,
                 curve: 1
@@ -197,10 +196,10 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'Threats - Wildfire',
-            image: './assets/buenos-aires.jpg',
+            image: './assets/caldor-fire.jpg',
             description: 'Buenos Aires, the capital of Argentina, is a dynamic city known for its European-inspired architecture, vibrant tango culture, and rich culinary scene. Often called the "Paris of South America," it blends historic charm with modern energy.  You can add as many chapters as you need, just copy the JSON data and make changes.',
             location: {
-                center: [-120.310040, 39.077802],
+                center: [-120.379528, 39.042268],
                 zoom: 9.5,
                 pitch: 0,
                 bearing: 0,
@@ -213,7 +212,7 @@ var config = {
                 {
                 layer: 'caldor-fire',
                 opacity: 1,
-                duration: 5000
+                duration: 3000
                 }
             ],
             onChapterExit: [  
